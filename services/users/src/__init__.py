5fd1a82beaf44953362ea3e233e1969352115c71
mradzikowski <radzik.radzikowski18@gmail.com>
2021-11-30 10:02:05 +0000

    Initialise the database in the app and create a simple user model

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -1,11 +1,29 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('src.config.DevelopmentConfig')
+# Taking application settings from dockerfile while running
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    date_created = db.Column(db.DateTime, default=func.now())
+    # TODO: Add more columns (think about database design)
 
 
 class Ping(Resource):
