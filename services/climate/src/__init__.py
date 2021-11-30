0ba6e53204e1303c4ae37c7ab781b9afbbf40212
mradzikowski <radzik.radzikowski18@gmail.com>
2021-11-30 20:20:13 +0000

    Add basic flask app to run and test microservices

---
+++ services/climate/src/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+# Taking application settings from dockerfile while running
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
