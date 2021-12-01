3895896a920a715276c16dc3d77d7a4569e2ed4b
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-01 12:41:23 +0000

    Change return message

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -29,7 +29,7 @@ class Ping(Resource):
     def get(self):
         return {
             "status": "success",
-            "message": "pong!",
+            "message": "pong from the users microservice!",
         }
 
 
