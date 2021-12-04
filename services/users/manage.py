eaca94cd783c03c18cc3de2bc4e3c06e05a51110
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-04 17:34:35 +0000

    Add flask cli command for seeding the database

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,13 +1,25 @@
 from flask.cli import FlaskGroup
-from src import app, db
+from src import create_app, db
+from src.api.models import User
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
+    db.session.commit()
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(username="Mateusz", email="mateusz@email.com"))
+    db.session.add(User(username="Toby", email="toby@email.com"))
+    db.session.add(User(username="Melissa", email="melissa@email.com"))
+    db.session.add(User(username="Joseph", email="joseph@email.com"))
+    db.session.add(User(username="Chloe", email="chloe@email.com"))
     db.session.commit()
 
 
