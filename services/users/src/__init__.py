8708b1aff7539e11d975ed524f5441362120df52
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-04 17:32:42 +0000

    Create a function for app creation and initializing the database

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -1,40 +1,27 @@
 import os
 
 from flask import Flask
-from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
 
-app = Flask(__name__)
-
-api = Api(app)
-
-# Taking application settings from dockerfile while running
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-class User(db.Model):
-    __tablename__ = "users"
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    date_created = db.Column(db.DateTime, default=func.now())
-    # TODO: Add more columns (think about database design)
+def create_app(script_info=None):
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
+    db.init_app(app)
 
-class Ping(Resource):
-    def get(self):
-        return {
-            "status": "success",
-            "message": "pong from the users microservice!",
-        }
+    from src.api.ping import ping_blueprint
+    from src.api.users import users_blueprint
 
+    app.register_blueprint(ping_blueprint)
+    app.register_blueprint(users_blueprint)
 
-api.add_resource(Ping, "/ping")
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
