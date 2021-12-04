88c5ed73a8604ef949508cf0f354dc08e27f04b5
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-04 17:34:53 +0000

    Move ping api to the api's folder

---
+++ services/users/src/api/ping.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask_restx import Api, Resource
+
+ping_blueprint = Blueprint("ping", __name__)
+api = Api(ping_blueprint)
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            "status": "success",
+            "message": "pong from the users microservice!",
+        }
+
+
+api.add_resource(Ping, "/ping")
