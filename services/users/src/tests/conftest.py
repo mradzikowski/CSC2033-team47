b626f370da4109d4ad5753496935229f4e69d27c
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-04 17:33:56 +0000

    Add pytest fixtures for initalising the test_app, test_database and a function to create a user

--- services/users/src/tests/conftest.py
+++ services/users/src/tests/conftest.py
@@ -1,9 +1,22 @@
 import pytest
-from src import app, db
+from src import create_app, db
+from src.api.models import User
+
+
+@pytest.fixture(scope="function")
+def add_user():
+    def _add_user(username, email):
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    return _add_user
 
 
 @pytest.fixture(scope="module")
 def test_app():
+    app = create_app()
     app.config.from_object("src.config.TestingConfig")
     with app.app_context():
         yield app
