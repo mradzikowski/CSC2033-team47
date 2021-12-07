9c49762a4623d90e129faeda1aaae0e96db6cc59
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-07 19:29:26 +0000

    Add password parementer due to adding the password entity for fixture in adding user

--- services/users/src/tests/conftest.py
+++ services/users/src/tests/conftest.py
@@ -5,8 +5,8 @@ from src.api.models import User
 
 @pytest.fixture(scope="function")
 def add_user():
-    def _add_user(username, email):
-        user = User(username=username, email=email)
+    def _add_user(username, email, password):
+        user = User(username=username, email=email, password=password)
         db.session.add(user)
         db.session.commit()
         return user
