c3726e37c68f8f9266551a02a9f4fd0c168b1223
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-07 19:26:36 +0000

    Add password hashing and decodeing/encoding the authorisation token using jwt

--- services/users/src/api/models.py
+++ services/users/src/api/models.py
@@ -1,5 +1,10 @@
+import datetime
+
+import jwt
+from flask import current_app
 from sqlalchemy.sql import func
 from src import db
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
@@ -7,16 +12,44 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False)
     twitter_link = db.Column(db.String(256), nullable=True)
     datasets = db.relationship("Dataset", backref="user", lazy=True)
     date_created = db.Column(db.DateTime, default=func.now())
-    # TODO: Add more columns (think about database design)
 
-    def __init__(self, username, email, twitter_link=None):
+    def __init__(self, username="", email="", password="", twitter_link=""):
         self.username = username
         self.email = email
+        self.password = generate_password_hash(password)
         self.twitter_link = twitter_link
+
+    def encode_auth_token(self, user_id, token_type):
+        if token_type == "access":
+            seconds = current_app.config.get("ACCESS_TOKEN_EXPIRATION")
+        else:
+            seconds = current_app.config.get("REFRESH_TOKEN_EXPIRATION")
+
+        payload = {
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=seconds),
+            "iat": datetime.datetime.utcnow(),
+            "sub": user_id,
+        }
+
+        return jwt.encode(
+            payload,
+            current_app.config.get("SECRET_KEY"),
+            algorithm="HS256",
+        )
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        payload = jwt.decode(
+            auth_token,
+            current_app.config.get("SECRET_KEY"),
+            algorithms="HS256",
+        )
+        return payload["sub"]
 
 
 class Dataset(db.Model):
