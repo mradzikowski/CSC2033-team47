266be04bc2bc32c785a07d62ff8084ee5d14c598
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-08 12:30:55 +0000

    Modify test due to use of flask_jwt_extended

--- services/users/src/tests/test_user_model.py
+++ services/users/src/tests/test_user_model.py
@@ -1,5 +1,4 @@
-import pytest
-from src.api.models import User
+from flask_jwt_extended import create_access_token
 
 
 def test_passwords_are_random(test_app, test_database, add_user):
@@ -8,15 +7,7 @@ def test_passwords_are_random(test_app, test_database, add_user):
     assert user_two.password != user_one.password
 
 
-@pytest.mark.parametrize("token_type", [["access"], ["refresh"]])
-def test_encode_token(test_app, test_database, add_user, token_type):
+def test_encode_token(test_app, test_database, add_user):
     user = add_user("thatsatest1", "test@test3.com", "simplepassword")
-    token = user.encode_auth_token(user.user_id, token_type)
+    token = create_access_token(identity=user.user_id)
     assert isinstance(token, str)
-
-
-def test_decode_token(test_app, test_database, add_user):
-    user = add_user("thatsatest1", "test@test3.com", "simplepassword")
-    token = user.encode_auth_token(user.user_id, "access")
-    assert isinstance(token, str)
-    assert User.decode_auth_token(token) == user.user_id
