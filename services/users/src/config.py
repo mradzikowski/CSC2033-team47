5020bcfb01f997a97e4a85e6e343286f0ba50bad
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-08 12:37:47 +0000

    Add config variables related to JWT library

--- services/users/src/config.py
+++ services/users/src/config.py
@@ -4,9 +4,9 @@ import os
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "my_secret_key"
-    ACCESS_TOKEN_EXPIRATION = 900
-    REFRESH_TOKEN_EXPIRATION = 2592000
+    JWT_SECRET_KEY = "my_secret_key"
+    JWT_ACCESS_TOKEN_EXPIRES = 900
+    JWT_REFRESH_TOKEN_EXPIRES = 2592000
 
 
 class DevelopmentConfig(BaseConfig):
@@ -16,8 +16,8 @@ class DevelopmentConfig(BaseConfig):
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_USERS_URL")
-    ACCESS_TOKEN_EXPIRATION = 3
-    REFRESH_TOKEN_EXPIRATION = 3
+    JWT_ACCESS_TOKEN_EXPIRES = 3
+    JWT_REFRESH_TOKEN_EXPIRES = 3
 
 
 class ProductionConfig(BaseConfig):
