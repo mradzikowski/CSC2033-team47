03e195c071a35c29362a4a8a2a3e0fcf7f349081
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-08 12:39:39 +0000

    Initialise the app with JWT manager and register auth blueprint

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 
 def create_app(script_info=None):
@@ -13,12 +15,15 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    jwt.init_app(app)
 
+    from src.api.auth import auth_blueprint
     from src.api.ping import ping_blueprint
     from src.api.users import users_blueprint
 
     app.register_blueprint(ping_blueprint)
     app.register_blueprint(users_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     @app.shell_context_processor
     def ctx():
