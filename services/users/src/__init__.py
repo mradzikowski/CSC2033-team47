516cbd9a6d66ce5d89e05d0bfc03b033e30032a8
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-09 19:54:02 +0000

    Add upload folder and max content length config for configuration

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -13,17 +13,22 @@ def create_app(script_info=None):
 
     app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
+    upload_folder = os.getenv("UPLOAD_FOLDER")
+    app.config["UPLOAD_FOLDER"] = upload_folder
+    app.config["MAX_CONTENT_LENGTH"] = 1000 * 1000
 
     db.init_app(app)
     jwt.init_app(app)
 
     from src.api.auth import auth_blueprint
+    from src.api.datasets import datasets_blueprint
     from src.api.ping import ping_blueprint
     from src.api.users import users_blueprint
 
     app.register_blueprint(ping_blueprint)
     app.register_blueprint(users_blueprint)
     app.register_blueprint(auth_blueprint)
+    app.register_blueprint(datasets_blueprint)
 
     @app.shell_context_processor
     def ctx():
