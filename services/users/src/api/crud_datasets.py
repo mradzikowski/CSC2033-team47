b92d100b9f3e60837925337a78b079b459ef613a
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-09 19:53:49 +0000

    Add upload folder and max content length config for configuration

---
+++ services/users/src/api/crud_datasets.py
@@ -0,0 +1,29 @@
+from src import db
+from src.api.models import Dataset
+
+
+def add_dataset(user_id, file_name, title, category):
+    dataset = Dataset(user_id, file_name, title, category)
+    db.session.add(dataset)
+    db.session.commit()
+    return dataset
+
+
+def get_all_datasets():
+    return Dataset.query.all()
+
+
+def get_dataset_by_id(dataset_id):
+    return Dataset.query.filter_by(dataset_id=dataset_id).first()
+
+
+def get_datasets_by_category(category):
+    return Dataset.query.filter_by(category=category).all()
+
+
+def get_dataset_by_title(title):
+    return Dataset.query.filter_by(title=title).all()
+
+
+def get_datasets_for_user(user_id):
+    return Dataset.query.filter_by(user_id=user_id).all()
