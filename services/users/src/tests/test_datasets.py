0406d23c229ba766f603015c5e567d528f979607
mradzikowski <radzik.radzikowski18@gmail.com>
2021-12-10 19:30:15 +0000

    Add tests for getting the datasets by category

--- services/users/src/tests/test_datasets.py
+++ services/users/src/tests/test_datasets.py
@@ -54,8 +54,9 @@ def test_upload_file_invalid_json_keys(
     assert status_code == resp_two.status_code
 
 
-def test_upload_file(test_app, test_database, add_user):
+def test_upload_file(test_app, test_database, add_user, add_category):
     user = add_user("Matty", "matty@email.com", "123456")
+    add_category("carbon-emission")
     client = test_app.test_client()
 
     resp_login = client.post(
@@ -91,14 +92,16 @@ def test_upload_file(test_app, test_database, add_user):
     )
 
     data = json.loads(resp_two.data.decode())
-    print(data)
 
     assert f"some_title has been uploaded by {user.user_id}" in data["message"]
     assert resp_two.status_code == 201
 
 
-def test_get_datasets(test_app, test_database, add_user, add_dataset):
+def test_get_datasets(test_app, test_database, add_user, add_dataset, add_category):
     test_database.session.query(Dataset).delete()
+    add_category("gas-emission")
+    add_category("gas-emission1")
+    add_category("gas-emission2")
 
     user = add_user("Matt", "matt@email.com", "123456")
     add_dataset(user.user_id, "file_name", "gas-emission", "gas-emission-title")
@@ -114,8 +117,6 @@ def test_get_datasets(test_app, test_database, add_user, add_dataset):
 
     data = json.loads(resp.data.decode())
 
-    print(data)
-
     assert len(data) == 3
     assert "file_name" in data[0]["file_name"]
     assert "file_name1" in data[1]["file_name"]
@@ -127,4 +128,38 @@ def test_get_datasets(test_app, test_database, add_user, add_dataset):
 
     assert "gas-emission-title" in data[0]["title"]
     assert "gas-emission-title1" in data[1]["title"]
+    assert "gas-emission-title2" in data[2]["title"]
+
+
+def test_get_datasets_by_category(
+    test_app,
+    test_database,
+    add_dataset,
+    add_user,
+    add_category,
+):
+    add_category("carbon")
+    user = add_user("Matty", "matty@email.com", "123456")
+    add_dataset(user.user_id, "file_name", "carbon", "gas-emission-title")
+    add_dataset(user.user_id, "file_name1", "carbon", "gas-emission-title1")
+    add_dataset(user.user_id, "file_name2", "carbon", "gas-emission-title2")
+
+    client = test_app.test_client()
+
+    resp = client.get(
+        "/datasets/category/carbon",
+        content_type="application/json",
+    )
+
+    data = json.loads(resp.data.decode())
+
+    assert len(data) == 3
+    assert "file_name" in data[0]["file_name"]
+    assert "carbon" in data[0]["category"]
+    assert "gas-emission-title" in data[0]["title"]
+    assert "file_name1" in data[1]["file_name"]
+    assert "carbon" in data[1]["category"]
+    assert "gas-emission-title1" in data[1]["title"]
+    assert "file_name2" in data[2]["file_name"]
+    assert "carbon" in data[2]["category"]
     assert "gas-emission-title2" in data[2]["title"]
