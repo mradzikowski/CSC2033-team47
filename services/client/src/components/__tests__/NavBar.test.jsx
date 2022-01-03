6bbb65882b1d773098f9397167bce24bee8e6eb0
mradzikowski <radzik.radzikowski18@gmail.com>
2022-01-03 09:19:40 +0000

    Fix failing tests due to change of styling

--- services/client/src/components/__tests__/NavBar.test.jsx
+++ services/client/src/components/__tests__/NavBar.test.jsx
@@ -15,18 +15,16 @@ describe("when unauthenticated", () => {
       isAuthenticated: jest.fn().mockImplementation(() => false),
     };
 
-    const { getByText, findByTestId } = renderWithRouter(<NavBar {...props} />);
-    expect(getByText(props.title)).toHaveClass("title");
+    const { findByTestId } = renderWithRouter(<NavBar {...props} />);
     await waitFor(() => {
       expect(props.isAuthenticated).toHaveBeenCalledTimes(1);
     });
-    expect((await findByTestId("nav-register")).innerHTML).toBe("Register");
-    expect((await findByTestId("nav-login")).innerHTML).toBe("Log In");
+    expect((await findByTestId("nav-register")).innerHTML).toBe("REGISTER");
+    expect((await findByTestId("nav-login")).innerHTML).toBe("LOG IN");
   });
 
   it("renders", () => {
     const props = {
-      title: "Hello, World!",
       logoutUser: () => {
         return true;
       },
@@ -40,19 +38,17 @@ describe("when unauthenticated", () => {
 describe("when authenticated", () => {
   it("renders the default props", async () => {
     const props = {
-      title: "Hello, World!",
       logoutUser: () => {
         return true;
       },
       isAuthenticated: jest.fn().mockImplementation(() => true),
     };
-    const { getByText, findByTestId } = renderWithRouter(<NavBar {...props} />);
-    expect(getByText(props.title)).toHaveClass("title");
+    const { findByTestId } = renderWithRouter(<NavBar {...props} />);
     await waitFor(() => {
       expect(props.isAuthenticated).toHaveBeenCalledTimes(1);
     });
-    expect((await findByTestId("nav-status")).innerHTML).toBe("Account");
-    expect((await findByTestId("nav-logout")).innerHTML).toBe("Log Out");
+    expect((await findByTestId("nav-status")).innerHTML).toBe("ACCOUNT");
+    expect((await findByTestId("nav-logout")).innerHTML).toBe("LOG OUT");
   });
 
   it("renders", () => {
