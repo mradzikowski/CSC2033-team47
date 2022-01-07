97f18f6d9d693f055e3a07553c158fc3f004132b
DESKTOP-PHGR2TP\Joseph <josephrhdk@gmail.com>
2022-01-07 15:17:53 +0000

    Added web scraper that gets values on Arctic Sea Ice Extent, Ice Sheets, Sea Level, Ocean Heat Added, Carbon Dioxide and Global Temperature from https://climate.nasa.gov/

--- services/users/src/api/crud/crud_climate_data.py
+++ services/users/src/api/crud/crud_climate_data.py
@@ -6,7 +6,6 @@ from helium import kill_browser, start_firefox
 from src import db
 from src.api.models import ClimateData
 
-
 def add_climate_data():
     url = "https://www.theworldcounts.com/challenges/climate-change"
     browser = start_firefox(url, headless=True)
@@ -29,6 +28,29 @@ def add_climate_data():
         return None
 
 
+# Retrieve data from https://climate.nasa.gov/
+def add_nasa_climate_data():
+    url = "https://climate.nasa.gov"
+    browser = start_firefox(url, headless=True)
+    time.sleep(1)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+
+    vitals = soup.find_all("div", {"class": "change_number"}, limit=6)
+
+    vitals = [vital.text for vital in vitals]
+
+    print(vitals)
+    return vitals
+
+    # Vitals corresponding meaning
+    # [0] - Arctic Sea Ice Extent (Percent per decade since 1979)(Down Arrow)
+    # [1] - Ice Sheets (Billion metric tons per year)(Down Arrow)
+    # [2] - Sea Level (Millimeters per year)(Up Arrow)
+    # [3] - Ocean Heat Added (Zettajoules since 1955)(Up Arrow)
+    # [4] - Carbon Dioxide (Parts per million (current))(Up Arrow)
+    # [5] - Global Temperature (Degrees centigrade since 1880)(Up Arrow)
+
+
 def get_climate_data_today():
     start_range = date.today()
     end_range = date.today() + timedelta(days=1)
@@ -43,3 +65,6 @@ def get_climate_data_today():
             return False
     else:
         return climate_data
+
+
+
