0c34e6360ae8534f80294c6265a51475f28cb568
DESKTOP-PHGR2TP\Joseph <josephrhdk@gmail.com>
2022-01-07 20:45:08 +0000

    Added web scraper that gets values on the metric tons of greenhouse emissions, november global temperature increase, arctic ice today vs the historic average, carbon free net power in the US and renewable power investments all from https://www.bloomberg.com/graphics/climate-change-data-green/

--- services/users/src/api/crud/crud_climate_data.py
+++ services/users/src/api/crud/crud_climate_data.py
@@ -6,6 +6,7 @@ from helium import kill_browser, start_firefox
 from src import db
 from src.api.models import ClimateData
 
+#Retrieve data from https://www.theworldcounts.com/challenges/climate-change
 def add_climate_data():
     url = "https://www.theworldcounts.com/challenges/climate-change"
     browser = start_firefox(url, headless=True)
@@ -35,20 +36,53 @@ def add_nasa_climate_data():
     time.sleep(1)
     soup = BeautifulSoup(browser.page_source, "html.parser")
 
+    # Get the first 6 div tags with the class change_number
     vitals = soup.find_all("div", {"class": "change_number"}, limit=6)
 
     vitals = [vital.text for vital in vitals]
-
     print(vitals)
+
+    kill_browser()
     return vitals
 
-    # Vitals corresponding meaning
+    # Vitals corresponding meaning (as of time of writing)
     # [0] - Arctic Sea Ice Extent (Percent per decade since 1979)(Down Arrow)
     # [1] - Ice Sheets (Billion metric tons per year)(Down Arrow)
     # [2] - Sea Level (Millimeters per year)(Up Arrow)
     # [3] - Ocean Heat Added (Zettajoules since 1955)(Up Arrow)
     # [4] - Carbon Dioxide (Parts per million (current))(Up Arrow)
     # [5] - Global Temperature (Degrees centigrade since 1880)(Up Arrow)
+
+
+# Retrieve data from https://www.bloomberg.com/graphics/climate-change-data-green/
+def add_bloomberg_data():
+    url = "https://www.bloomberg.com/graphics/climate-change-data-green/"
+    browser = start_firefox(url, headless=True)
+    time.sleep(1)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+
+    # Get div elements
+    # Ignore values 0 and 4
+    data = soup.find_all("div", {"class": "number"})
+    # Ignore values 0 and 4 as not needed
+    data.pop(4)
+    data.pop(0)
+    # Convert values to text form
+    data = [datum.text for datum in data]
+
+    # For dev purposes
+    for datum in data:
+        print(datum)
+
+    # Data corresponding meaning (as of time of writing)
+    # [0] - Million metric tons of greenhouse emissions, most recent annual data
+    # [1] - November 2021 increase in global temperature vs 1900s average
+    # [2] - Today's arctic ice area vs. historic average
+    # [3] - Carbon-free net power in the United States, most recent data
+    # [4] - Renewable power investment worldwide in Q2 2020
+    kill_browser()
+
+    return data
 
 
 def get_climate_data_today():
