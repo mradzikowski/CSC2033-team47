e2b296eb45b37f47483b012aa62df8f8fbad9fee
mradzikowski <radzik.radzikowski18@gmail.com>
2022-01-12 11:37:31 +0000

    Add twitter data namespace to the api namesapce

--- services/users/src/api/__init__.py
+++ services/users/src/api/__init__.py
@@ -2,6 +2,7 @@ from flask_restx import Api
 from src.api.auth.views import auth_namespace
 from src.api.climate_data.views import climate_data_namespace
 from src.api.datasets.views import datasets_namespace
+from src.api.tweets.views import twitter_data_namespace
 from src.api.users.views import users_namespace
 
 api = Api(version="1.0", title="API backend", doc="/doc")
@@ -10,3 +11,4 @@ api.add_namespace(auth_namespace, "/auth")
 api.add_namespace(datasets_namespace, "/datasets")
 api.add_namespace(users_namespace, "/users")
 api.add_namespace(climate_data_namespace, "/climatedata")
+api.add_namespace(twitter_data_namespace, "/tweets")
