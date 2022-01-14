d49c60a325e812beff9f6679886aa7902ac9f512
Toby Dixon <thetjdixon1234@gmail.com>
2022-01-14 00:01:05 +0000

    fixed issue with world counters

--- services/client/src/components/ClimateData.js
+++ services/client/src/components/ClimateData.js
@@ -20,26 +20,8 @@ class ClimateData extends React.Component {
   }
 
   componentDidMount() {
-
-    // Get World Counts
-    fetch(`${process.env.REACT_APP_USERS_SERVICE_URL}/climatedata/worldcounts`)
-      .then((res) => res.json())
-      .then((values) => {
-        let worldCounts = [];
-
-        for (let key in values[0]) {
-          if (key != "id")
-            worldCounts.push({ title: key, data: values[0][key] });
-        }
-
-        this.setState({ worldCounts: worldCounts });
-        this.setState({ retrieved: true });
-      });
-      
-      this.getCounterData();
+    this.getCounterData();
   }
-
-
 
   getCounterData() {
     // Get World Counts
